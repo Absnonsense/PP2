c1dbc5b257ac7da3555e5af37d41f03ff58f4e06
Absnonsense <alikhanzhanymkhanov@gmail.com>
2025-02-08 03:49:48 +0500

    Create Class_1.py

---
+++ Class_1.py
@@ -0,0 +1,13 @@
+class up:
+  def __init__(self):
+      self.input_string = "" 
+
+  def getString(self):
+      self.input_string = input("Enter a string: ")
+
+  def printString(self):
+      print(self.input_string.upper())
+
+up = up()
+up.getString()
+up.printString()
