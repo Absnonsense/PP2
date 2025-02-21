f565fefef5ae6325c1ec4fc9cc98ac8eb379eb51
Absnonsense <alikhanzhanymkhanov@gmail.com>
2025-02-21 21:48:01 +0500

    Update Date_3.py

--- Date_3.py
+++ Date_3.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-
-current_datetime = datetime.now()
-datetime_without_microseconds = current_datetime.replace(microsecond=0)
-
-print("Datetime with microseconds:", current_datetime)
+user_input = input("Enter a datetime (YYYY-MM-DD HH:MM:SS.microseconds): ")
+user_datetime = datetime.strptime(user_input, "%Y-%m-%d %H:%M:%S.%f")
+datetime_without_microseconds = user_datetime.replace(microsecond=0)
 print("Datetime without microseconds:", datetime_without_microseconds)
