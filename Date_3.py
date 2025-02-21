836dc8df02e0f26a065847fdab6329cc0f9eb296
Absnonsense <alikhanzhanymkhanov@gmail.com>
2025-02-21 21:42:17 +0500

    Create Date_3.py

---
+++ Date_3.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+current_datetime = datetime.now()
+datetime_without_microseconds = current_datetime.replace(microsecond=0)
+
+print("Datetime with microseconds:", current_datetime)
+print("Datetime without microseconds:", datetime_without_microseconds)
